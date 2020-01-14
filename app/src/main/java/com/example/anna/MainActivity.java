f1e4af69738f874e6cb4316298a7be4c2a861274
ndkhanh360 <duykhanhnguyen360@gmail.com>
2020-01-14 17:13:22 +0700

    edit topic list interface

--- app/src/main/java/com/example/anna/MainActivity.java
+++ app/src/main/java/com/example/anna/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -44,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
 }
